252199462224945decb8516ec1489f0d57d82106
romain344 <romainleoulzec@gmail.com>
2025-05-07 12:50:33 +0200

    progarme print the the ASCII alphabet

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
-#!/usr/pyton3
-for a in range (97, 123
-                if a != 101 and a != 113:
-                print("{:c".forma(i), end="")
+#!/usr/bin/python3
+for a in range(97, 123):
+    if a != 101 and a != 113:
+        print("{:c}".format(a), end="")
