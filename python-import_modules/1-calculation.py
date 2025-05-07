61df7e10a3f5f6f65a743efa476971af980c6395
romain344 <romainleoulzec@gmail.com>
2025-05-07 15:39:25 +0200

    importe ces fonctions et affiche les résultats.

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-from calculator_1
+from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     a = 10
     b = 5
 
-    result_add = calculator_1.add(a, b)
-    result_sub = calculator-1.subtract(a,b)
-    result_mul = calculator_1.multiply(a, b)
-    result_div = calculator_1.divide(a, b)
-
-    print("Addition :", result_add)
-    print("Soustraction :", result_sub)
-    print("Multiplication :", result_mul)
-    print("Division :", result_div)
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
