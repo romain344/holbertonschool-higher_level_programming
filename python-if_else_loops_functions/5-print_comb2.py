552fe951d69dea33b503024b7f007a4f2073bdbd
romain344 <romainleoulzec@gmail.com>
2025-05-07 11:57:00 +0200

    Write a program that prints numbers from 0 to 99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for a in range(0, 100):
-    if a != 99:
-        print(f"{a:02}", end=", ")
-    else:
-        print(f"{a:02}")
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
