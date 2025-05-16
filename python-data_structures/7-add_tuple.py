9bc32d9b9bc362d9b5d6a41b84d35832ba1ce244
romain344 <romainleoulzec@gmail.com>
2025-05-16 11:04:56 +0200

    aditione les 2 tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        tuple_a = tuple_a + (0,) * (2 - len(tuple_a))
-    else:
-        tuple_a = tuple_a[:2] 
-    if len(tuple_b) < 2:
-        tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
-    else:
-        tuple_b = tuple_b[:2]
-    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return result
+    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
+
+    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    b2 = tuple_b[1] if len(tuple_b) > 1 else 0
+
+    return (a1 + b1, a2 + b2)
