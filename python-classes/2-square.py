0ca6877e56fd6f1cd7a295ca28ed31ce9a6d4a30
romain344 <romainleoulzec@gmail.com>
2025-05-23 14:41:28 +0200

    function checks if size of class Square is an integer

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""define la class"""
+
+
 class Square:
+    """class Square"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError ("size must be an integer")
