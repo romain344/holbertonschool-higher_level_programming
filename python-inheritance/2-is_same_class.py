254fcee9342226f8289b7ef4f6b62619f89a494c
romain <romainleoulzec@gmail.com>
2025-05-29 14:39:43 +0200

    class MyList that inherits from list

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Defines an object attribute lookup function"""
+
+
 def is_same_class(obj, a_class):
     """Check if an object is exactly an instance of a specified class"""
     if type(obj) is a_class:
         return True
     else:
         return False
+    
