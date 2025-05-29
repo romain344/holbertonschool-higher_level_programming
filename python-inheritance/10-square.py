3be4a4a3eee8ce768e894409837a1550c64dce39
romain <romainleoulzec@gmail.com>
2025-05-29 14:24:38 +0200

    eureeur

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Square class definition with area method """
+Rectangle = __import__('9-rectangle').Rectangle
+class Square(Rectangle):
+    """Square class that inherits from Rectangle"""
+    
---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/phython3
+""" Rectangle class definition with area method """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+rectangle = __import__('8-rectangle').Rectangle
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        self .integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+    def area(self):
+        area  = rectangle.__width * rectangle.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
