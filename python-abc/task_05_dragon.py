7e6a80bb49e27a258f98cd4067329f3c9449598a
romain <romainleoulzec@gmail.com>
2025-05-30 15:41:53 +0200

    Implement SwimMixin, FlyMixin, and Dragon class with swim, fly, and roar methods

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Task 05 - The Mystical Dragon - Mastering Mixins"""
+
+class SwimMixin:
+    def swim(self):
+        print("The creature swims!")
+
+class FlyMixin:
+    def fly(self):
+        print("The creature flies!")
+
+class Dragon(SwimMixin, FlyMixin):
+    def roar(self):
+        print("The dragon roars!")
