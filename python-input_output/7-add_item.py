752f15111023855bb1ee003871cf8ef0cb9a71d8
romain <romainleoulzec@gmail.com>
2025-06-05 21:09:54 +0200

    Create script to append command line arguments to JSON list file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,9 +2,9 @@
 """Script that adds all command line arguments to a JSON list file."""
 
 import sys
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 
 def main():
     filename = "add_item.json"
