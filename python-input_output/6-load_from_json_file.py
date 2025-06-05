d3ae7ff32611427091b8ad216470e8880a774eb9
romain <romainleoulzec@gmail.com>
2025-06-05 21:04:42 +0200

    Add function to load Python object from JSON file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Write a file with some text and then read it back to verify its contents"""
+
+
 import json
+
 
 def load_from_json_file(filename):
     """Creates an object from a JSON file"""
