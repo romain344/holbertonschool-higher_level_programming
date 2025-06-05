713e1244ad5609d7b21754b8577c603bef5f60c5
romain <romainleoulzec@gmail.com>
2025-06-05 20:59:47 +0200

    Add function to convert JSON string to Python object

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """Write a file with some text and then read it back to verify its contents"""
+
+
 import json
+
+
 def from_json_string(my_str):
     """Returns the Python object represented by a JSON string"""
 
