987930b1b19e937ae4aa9b742e8d668eaff000f3
romain <romainleoulzec@gmail.com>
2025-06-05 21:16:33 +0200

    Add Student class with to_json method for dictionary serialization

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""Script that converts a class instance to a JSON object."""
 
 
 class Student:
     """define a student class"""
+
     def __init__(self, first_name, last_name, age):
-        """initialize the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self, attrs=None):
         return self.__dict__
