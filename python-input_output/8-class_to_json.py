20d5132e7c7c7dc1ad38e3ba113370601b53c169
romain <romainleoulzec@gmail.com>
2025-06-05 21:12:25 +0200

    Add function to convert class instance attributes to JSON-serializable dictionary

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Script that converts a class instance to a JSON object."""
 
 
 def class_to_json(obj):
+    """Returns the dictionary description with simple data structure"""
+
     obj_dict = obj.__dict__
     return __dict__
