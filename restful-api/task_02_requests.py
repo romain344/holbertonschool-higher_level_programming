eb6be020d264313b6f9807c3f21787518cdacb58
romain <romainleoulzec@gmail.com>
2025-06-15 09:45:45 +0200

    Fetch and process posts data from JSONPlaceholder API

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import requests
+import csv
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post.get("title"))
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+        formatted_posts = [
+            {
+                "id": post.get("id"),
+                "title": post.get("title"),
+                "body": post.get("body")
+            }
+            for post in posts
+        ]
+
+        with open("posts.csv", mode="w", newline="", encoding="utf-8") as csvfile:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(formatted_posts)
