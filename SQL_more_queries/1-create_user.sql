dca0575c6d0f7e2a38fd89802031f36d08667416
romain <romainleoulzec@gmail.com>
2025-06-25 17:25:52 +0200

    created user and give all privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- created user user_0d_1 with all privileges
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
