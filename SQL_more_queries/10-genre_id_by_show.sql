54f83d5c072e1c9095983c1155aeaa8fd7149c80
romain <romainleoulzec@gmail.com>
2025-06-27 16:18:44 +0200

    list a tv

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- lsite les tv
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres
-WHERE tv_shows.id = tv_show_genres.tv_show_id
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
