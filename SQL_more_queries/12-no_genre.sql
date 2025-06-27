52223a4a8a85b514a9300801025d0510e5f3c889
romain <romainleoulzec@gmail.com>
2025-06-27 16:23:53 +0200

    List shows without genre: select shows with NULL genre_id, sorted by title and genre_id

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
---
+-- liste les emissions sans genre
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
