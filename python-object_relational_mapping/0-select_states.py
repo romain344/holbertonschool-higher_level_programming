7b4f90e27a893c4ac21d9976ac0e51b1094c8919
romain <romainleoulzec@gmail.com>
2025-06-27 17:04:29 +0200

    list all states from database hbtn_0e_0_usa using MySQLdb

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-""" import SQL et SYS """
+""" Script that lists all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    """ recupére les arguments de la ligne de commande """
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    """ connecte à la base de données """
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -20,17 +18,10 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-
-
-    """ execute la requête SQL """
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-    """ récupère les résultats """
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    
-    """ ferme le curseur et la connexion """
     cur.close()
     db.close()
     
