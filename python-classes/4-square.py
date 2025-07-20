d45b68c1d0341af6d80736843c2aa4d5603a27c0
romain344 <romainleoulzec@gmail.com>
2025-07-20 19:23:32 +0200

    task4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""define la class"""
+
+
 class Square:
+    """class square"""
     def __init__(self, size=0):
         self.size = size
     @property
@@ -12,5 +16,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
+        """return the area of the square"""
         return self.__size ** 2
