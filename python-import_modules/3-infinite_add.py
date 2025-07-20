5383d62b526dda4543c293380ab9d51ed5a6081b
romain <romainleoulzec@gmail.com>
2025-07-20 14:22:28 +0200

    The program allows to differentiate between positive and negative numbers

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-
+if __name__ == '__main__':
+    from sys import argv
 
 if __name__ == '__main__':
     total = 0
     for number in range(1, len(argv)):
          number = int(argv[number])
          total += number
-
     print(total)
