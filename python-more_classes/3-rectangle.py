661e35caa3ac6f8edf2c442476439588c019cb86
romain <romainleoulzec@gmail.com>
2025-07-27 15:02:55 +0200

    add string representation to Rectangle using

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Rectangle class module"""
+
+
 class Rectangle:
+    """"Defines a rectangle with area and perimeter."""
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -10,6 +15,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        """Function to set the new width value"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -22,6 +28,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """Function to set the new height value"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -29,17 +36,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        return self.__width * self.__height
+        """return the area of the rectangle"""
+        return self.width * self.height
 
     def perimeter(self):
-        if self.__width == 0 or self.__height == 0:
+        """return the perimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
-
-    def __str__(self):
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        line = []
-        for i in range(self.__height):
-            line.append("#" * self.__width)
-        return "\n".join(line)
+        return 2 * (self.width + self.height)
