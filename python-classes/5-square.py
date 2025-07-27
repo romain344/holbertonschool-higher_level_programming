16cef08b7bbd6943ca9ab6005e3217e434073995
romain <romainleoulzec@gmail.com>
2025-07-27 14:35:03 +0200

    task

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
-
 class Square:
     """represent a square"""
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size  # Utilise le setter pour validation
 
     @property
     def size(self):
         """get the size of the square"""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         """set the size of the square"""
@@ -19,11 +18,14 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        def area(self):
-            return self.__size ** 2
-        
+        self.__size = value
+
+    def area(self):
+        """return the current square area"""
+        return self.__size ** 2
+
     def my_print(self):
-        """Print the square with the character """
+        """Print the square with the character #"""
         if self.__size == 0:
             print()
         else:
