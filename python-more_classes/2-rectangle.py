fcfbaad20dc79b9175accac69f7030247dbcbad5
romain <romainleoulzec@gmail.com>
2025-07-27 14:58:58 +0200

    add fonction area

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""define a class rectangle"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """defina a rectangle class"""
+    """Defines a rectangle with area and perimeter."""
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """get the width of the rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """set the width od the rectangle """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -24,12 +23,10 @@ class Rectangle:
 
     @property
     def height(self):
-        """get the height of the rectangle"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
-        """set the height of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -37,11 +34,9 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """return the area of the rectangle"""
         return self.__width * self.__height
-    
+
     def perimeter(self):
-        """return the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.__width + self.__height) * 2
+        return 2 * (self.__width + self.__height)
