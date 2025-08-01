fc21831124b7a7a51d53446f01e83758e4a7a61f
romain344 <romainleoulzec@gmail.com>
2025-08-01 13:26:05 +0200

    display the value

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         db=db_name
     )
     cursor = db.cursor()
-    query = ("SELECT * FROM states WHERE name = '{}' "
+    query = ("SELECT * FROM states WHERE BINARY name = '{}' "
              "ORDER BY id ASC").format(state_name)
     cursor.execute(query)
     for row in cursor.fetchall():
