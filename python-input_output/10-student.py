84ce1b85427ef7dd599dd63d6a69b3398a6a58f4
romain344 <romainleoulzec@gmail.com>
2025-08-01 18:21:50 +0200

    class Student that defines a student by: (based on 9-student.py)

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -6,12 +6,14 @@ class Student:
     """define a student class"""
 
     def __init__(self, first_name, last_name, age):
+        """initialize the student instance"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """Returns the dictionary description with simple data structure"""
         if isinstance(attrs, list) and all(type(a) == str for a in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         else:
-            return self.__dict__.copy()
+            return self.__dict__.copy() 
