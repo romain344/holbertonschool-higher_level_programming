8c900a91df3fb57df34d93952d18eee6de50838a
romain344 <romainleoulzec@gmail.com>
2025-08-02 16:37:53 +0200

    lsit

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" défine la fonction"""
+
+
 def roman_to_int(roman_string):
+    """converie une chaîne de caractères en entier"""
     if not isinstance(roman_string, str):
         return 0
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
