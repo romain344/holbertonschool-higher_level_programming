641764a43408b3ddd35f9e5a69b58531cd3ef094
romain344 <romainleoulzec@gmail.com>
2025-08-02 16:40:14 +0200

    lsit

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python3
-""" défine la fonction"""
+"""défine la fonction"""
 
 
 def roman_to_int(roman_string):
-    """converie une chaîne de caractères en entier"""
-    if not isinstance(roman_string, str):
+    """converties le nombre romain en entier"""
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
     total = 0
-    for i in range(len(roman_string)):
-        if i + 1 < len(roman_string) and roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]:
-            total -= roman_dict[roman_string[i]]
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        value = roman_dict.get(char)
+        if value is None:
+            return 0
+        if value < prev_value:
+            total -= value
         else:
-            total += roman_dict[roman_string[i]]
+            total += value
+        prev_value = value
+
     return total
